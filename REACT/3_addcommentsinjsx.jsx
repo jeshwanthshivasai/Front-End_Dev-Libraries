6b9f4cc99a7e84d7abafbfe338a60d853914f662
Jeshwanth Shiva Sai <havingthesensesofaspider@gmail.com>
2024-09-29 20:56:43 +0530

    3

---
+++ REACT/3_addcommentsinjsx.jsx
@@ -0,0 +1,14 @@
+//JSX is a syntax that gets compiled into valid JavaScript.
+//Sometimes, for readability, you might need to add comments to your code.
+//Like most programming languages, JSX has its own way to do this.
+
+//To put comments inside JSX, you use the syntax 
+{/*hello world*/} //to wrap around the comment text.
+
+const JSX = (
+    <div>
+      <h1>This is a block of JSX</h1>
+      <p>Here's a subtitle</p>
+      {/*hello world*/}
+    </div>
+);

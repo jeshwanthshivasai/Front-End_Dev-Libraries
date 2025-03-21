139a8e60fe482fd679761306922e959791be361d
jeshwanthshivasai <havingthesensesofaspider@gmail.com>
2025-03-21 12:20:14 +0530

    3/17

---
+++ REDUX/defineareduxaction.js
@@ -0,0 +1,6 @@
+// Writing a Redux action is as simple as declaring an object with a type property.
+// Declare an object action and give it a property type set to the string 'LOGIN'.
+
+const action = {
+    type: 'LOGIN'
+}
